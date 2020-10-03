42eaa760e26750cc1a636f8b18ad9ea1c098a7bf
svasandani <vasandanishailesh@gmail.com>
2020-10-03 10:36:19 -0400

    Responsive concerns

--- preloader.js
+++ preloader.js
@@ -6,11 +6,13 @@ let preloader = document.querySelector(".preloader");
 
 window.addEventListener('load', () => {
     preloader.classList.add("loaded");
-    
-    toSlide.forEach((t) => {
-        setTimeout(() => { t.classList.remove("unslided"); t.classList.add("slided"); }, 500);
-    });
-
     document.body.classList.remove("unloaded");
     document.body.classList.add("loaded");
+    
+    toSlide.forEach((t) => {
+        setTimeout(() => { 
+            t.classList.remove("unslided"); t.classList.add("slided"); 
+        }, 600);
+    });
+
 });
